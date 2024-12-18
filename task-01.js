664e997734d7557a3a8f69213486b864efc1212d
OCTO <chobotnicecz@gmail.com>
2024-12-18 15:05:51 +0100

    task - 01

---
+++ task-01.js
@@ -0,0 +1,37 @@
+let arr = [];
+let length;
+let element;
+
+do {
+  length = parseInt(prompt("Enter quantity of items"));
+} while (length < 1 || isNaN(length) || length === null);
+
+if (length === 1) {
+  console.log("\nYou list have just ONE item\n ");
+} else {
+  console.log("\nYou list have: " + length + " items\n ");
+}
+
+for (let i = 0; i < length; i++) {
+  do {
+    element = prompt("Enter your element");
+  } while (element === null);
+  arr.push(element);
+}
+
+console.log("\nYour list: " + arr.join(" "));
+
+arr.sort();
+
+console.log("\nSorted list: " + arr.join(" ") + "\n ");
+
+if (length >= 4) {
+  arr.splice(1, 3);
+} else {
+  alert(
+    "Your example is not suitable for demonstration splice().\nEnter the number of elements greater than 4."
+  );
+}
+
+console.log("Your finale list: " + arr.join(" "));
+console.log("Array lenght: " + arr.length);
